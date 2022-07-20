f6bcd5bb6132a7f5ff85a7a40f0ce73583f62b07
pk-14 <100840901+pk-14@users.noreply.github.com>
2022-07-20 14:51:58 +0530

    User Model, Auth

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const app = express()
 
+const cookieParser = require('cookie-parser');
+
 const errorMiddleware = require('./middleware/error');
 
 app.use(express.json())
+app.use(cookieParser())
 
 // Routes
 const product = require("./routes/productRoute")
-app.use("/api/v1",product)
+const user = require('./routes/userRoutes');
+
+app.use("/api/v1", product)
+app.use("/api/v1", user)
 
 // Middleware
 app.use(errorMiddleware)
