8b9befffde7926cd3585280b693ceee19ef7f7cd
pk-14 <100840901+pk-14@users.noreply.github.com>
2022-07-20 21:16:20 +0530

    User, Review CRUD

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 const User = require('../models/userModel');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const cloudinary = require("cloudinary");
+const crypto = require("crypto");
 
 // Register a User
 exports.registerUser = catchAsyncErrors( async (req, res, next) => {
@@ -135,4 +137,116 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save();
   
     sendToken(user, 200, res);
+});
+
+// Get User Detail
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  })
+})
+
+// Update User password
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password")
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword)
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Old password is incorrect!", 400))
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHander("Passwords does not match!", 400))
+  }
+
+  user.password = req.body.newPassword
+
+  await user.save()
+
+  sendToken(user, 200, res)
+})
+
+// Update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Get all users(admin)
+exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+// Get single user (admin)
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHander(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delete User --Admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHander(`User does not exist with Id: ${req.params.id}`, 400)
+    );
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully!",
+  });
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,20 @@
 const express = require('express')
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require('../controllers/userController')
+const { 
+    registerUser, 
+    loginUser, 
+    logout, 
+    forgotPassword, 
+    resetPassword, 
+    getUserDetails, 
+    updatePassword,
+    updateProfile,
+    getAllUser,
+    getSingleUser,
+    updateUserRole,
+    deleteUser,
+} = require('../controllers/userController')
 const router = express.Router()
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth')
 
 router.route("/register").post(registerUser)
 
@@ -11,5 +25,21 @@ router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
 
 router.route("/logout").get(logout)
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails)
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser)
+
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser)
 
 module.exports = router
